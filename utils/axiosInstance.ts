77f0517355372ee8027821efa3035ba1a6948a71
Prasai1488 <prasaiprithvi4@gmail.com>
2025-01-06 21:30:58 +0545

    minor css changes

--- utils/axiosInstance.ts
+++ utils/axiosInstance.ts
@@ -1,9 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: 'https://api.themoviedb.org/3', // Base URL for API
+  baseURL: "https://api.themoviedb.org/3", // Base URL for API
   headers: {
-    Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxY2ViYTE1YzhlOTMwNmExNGMxZWQ3ZDUyYTRlNGFhMCIsIm5iZiI6MTczMjYxMjEwNC4xMTAzNDA0LCJzdWIiOiI2NzQ1OGRkMzgwYjQ0YTg5MzdiN2MzNDUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.f5pYhZOw9kt_ZFyPzWay-D1seZ2dOGJ43W7Mb5-a-A0`,
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_TMDB_API_KEY}`,
   },
 });
 
